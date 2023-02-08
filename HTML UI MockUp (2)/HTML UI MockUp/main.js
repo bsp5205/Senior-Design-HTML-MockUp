84561ffafdc936aa589fa38c2412eb929ce41ead
bsp13 <bsp5205@psu.edu>
2023-02-08 14:12:28 -0500

    Reformatted entire project and added some basic comments

--- HTML UI MockUp (2)/HTML UI MockUp/main.js
+++ HTML UI MockUp (2)/HTML UI MockUp/main.js
@@ -18,136 +18,179 @@ app.listen(port, () => {
 class Student {
     name = '';
     studentID = '';
-    submissions = [];
+    submission;
     submission_time = '';
 }
 
 class Submission {
     name = '';
-    file_code = '';
-    QMA_scores = [];
-    QMA_names = [];
-    CMA_scores = [];
-    CMA_names = [];
+    submission_time = '';
+    file_list = [];
 }
 
+class Submission_File{
+    code = ''
+    QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
+    CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage'];
+}
+
+let student_1 = new Student();
+student_1.name = 'Student 1';
+student_1.studentID = '123';
+
+let student_1_submission = new Submission();
+student_1_submission.submission_time = 'December 30, 2022 at 9:37PM';
+
+let file_1 = new Submission_File();
+file_1.name = 'Test';
+file_1.id = '1';
+file_1.code = '#include <iostream>\n' +
+    '\n' +
+    'int main() {\n' +
+    '    std::cout << "Hello World!";\n' +
+    '    return 0;\n' +
+    '}'
+
+file_1.QMA_scores = [90, 65, 10, 35];
+file_1.CMA_scores = [100, 80, 65];
+
+let file_2 = new Submission_File();
+file_2.name = 'Test 2'
+file_2.id = '2'
+file_2.code = '#include <iostream>\n' +
+    '\n' +
+    'int main() {\n' +
+    '    std::cout << "Hello from Class 2";\n' +
+    '    return 0;\n' +
+    '}'
+file_2.QMA_scores = [0, 40, 10, 5];
+file_2.CMA_scores = [87, 55, 40];
+
+student_1_submission.file_list.push(file_1);
+student_1_submission.file_list.push(file_2);
+
+student_1.submission = student_1_submission;
+
+let student_list = [];
+student_list.push(student_1);
+
+let student_2 = new Student();
+student_2.name = 'Student 2';
+student_1.studentID = '456';
+let student_2_submission = new Submission();
+student_2_submission.submission_time = 'December 29, 2022 at 2:15PM';
+
+let file_12 = new Submission_File();
+file_12.name = 'Student 2 Test';
+file_12.id = '1';
+file_12.code = '#include <iostream>\n' +
+    '\n' +
+    'int main() {\n' +
+    '    std::cout << "I am Student 2, Hello World!";\n' +
+    '    return 0;\n' +
+    '}'
+file_12.QMA_scores = [100, 33, 67, 12];
+file_12.CMA_scores = [34, 56, 76];
+
+let file_23 = new Submission_File();
+file_23.name = 'Student 2 Test 2'
+file_23.id = '2'
+file_23.code = '#include <iostream>\n' +
+    '\n' +
+    'int main() {\n' +
+    '    std::cout << "I am Student 2, Hello from Class 2";\n' +
+    '    return 0;\n' +
+    '}'
+file_23.QMA_scores = [45, 67, 43, 43];
+file_23.CMA_scores = [43, 56, 78];
+
+student_2_submission.file_list.push(file_12);
+student_2_submission.file_list.push(file_23);
+
+student_2.submission = student_2_submission;
+
+student_list.push(student_2)
+
 app.get("/", function(req,res){
-    let student1 = new Student()
-    student1.name = 'Student 1'
-    student1.studentID = '123'
-    student1.submission_time = 'January 1, 2023 at 11:58PM'
+    // let student1 = new Student()
+    // student1.name = 'Student 1'
+    // student1.studentID = '123'
+    // student1.submission_time = 'January 1, 2023 at 11:58PM'
+    //
+    // let s1s1 = new Submission()
+    // s1s1.name = 'Homework 1'
+    //
+    // s1s1.file_code = '#include <iostream>\n' +
+    //     '\n' +
+    //     'int main() {\n' +
+    //     '    std::cout << "Hello World!";\n' +
+    //     '    return 0;\n' +
+    //     '}'
+    //
+    // s1s1.QMA_scores = [90, 65, 10, 35];
+    // s1s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
+    // s1s1.CMA_scores = [100, 80, 65]
+    // s1s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
+    // student1.submissions.push(s1s1)
+    //
+    // let student2 = new Student()
+    // student2.name = 'Student 2'
+    // student2.studentID = '456'
+    // student2.submission_time = 'December 30, 2022 at 9:37PM'
+    //
+    // let s2s1 = new Submission()
+    // s2s1.name = 'Homework 1 File'
+    //
+    // s2s1.file_code = 'class HelloWorld {\n' +
+    //     '    public static void main(String[] args) {\n' +
+    //     '        System.out.println("Hello, World!"); \n' +
+    //     '    }\n' +
+    //     '}'
+    //
+    // s2s1.QMA_scores = [10, 20, 50, 25];
+    // s2s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
+    // s2s1.CMA_scores = [45, 30, 15]
+    // s2s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
+    // student2.submissions.push(s2s1)
+    //
+    // student1.submissions.push(s2s1)
+    // student1.submissions.push(s2s1)
+    // let s1 = ['50','50','50','50','50','50','50']
+    // let student_list = [];
+    // student_list.push(student1)
+    // student_list.push(student2)
 
-    let s1s1 = new Submission()
-    s1s1.name = 'Homework 1'
-
-    s1s1.file_code = '#include <iostream>\n' +
-        '\n' +
-        'int main() {\n' +
-        '    std::cout << "Hello World!";\n' +
-        '    return 0;\n' +
-        '}'
-
-    s1s1.QMA_scores = [90, 65, 10, 35];
-    s1s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
-    s1s1.CMA_scores = [100, 80, 65]
-    s1s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
-    student1.submissions.push(s1s1)
-
-    let student2 = new Student()
-    student2.name = 'Student 2'
-    student2.studentID = '456'
-    student2.submission_time = 'December 30, 2022 at 9:37PM'
-
-    let s2s1 = new Submission()
-    s2s1.name = 'Homework 1 File'
-
-    s2s1.file_code = 'class HelloWorld {\n' +
-        '    public static void main(String[] args) {\n' +
-        '        System.out.println("Hello, World!"); \n' +
-        '    }\n' +
-        '}'
-
-    s2s1.QMA_scores = [10, 20, 50, 25];
-    s2s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
-    s2s1.CMA_scores = [45, 30, 15]
-    s2s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
-    student2.submissions.push(s2s1)
-
-    student1.submissions.push(s2s1)
-    student1.submissions.push(s2s1)
-
-    let student_list = [];
-    student_list.push(student1)
-    student_list.push(student2)
-    res.render("index.ejs", {selected_student:student_list[0], student_list:student_list});
+    res.render("index.ejs", {selected_student:student_list[0], focus_file:student_1_submission.file_list[1],student_list:student_list});
 });
 
 app.post('/change_student', (req, res) =>{
     let id = req.body.student_list;
     console.log(id)
-    let s1 = req.body.test;
-    console.log(s1)
-
-    let student1 = new Student()
-    student1.name = 'Student 1'
-    student1.studentID = '123'
-    let s1s1 = new Submission()
-    s1s1.name = 'Homework 1'
-
-    s1s1.file_code = '#include <iostream>\n' +
-        '\n' +
-        'int main() {\n' +
-        '    std::cout << "Hello World!";\n' +
-        '    return 0;\n' +
-        '}'
-
-    student1.submission_time = 'January 1, 2023 at 11:58PM'
-    s1s1.QMA_scores = [90, 65, 10, 50];
-    s1s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
-    s1s1.CMA_scores = [100, 80, 65]
-    s1s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
-    student1.submissions.push(s1s1)
-
-    let student2 = new Student()
-    student2.name = 'Student 2'
-    student2.studentID = '456'
-    let s2s1 = new Submission()
-    s2s1.name = 'Homework 1 File'
-
-    s2s1.file_code = 'class HelloWorld {\n' +
-        '    public static void main(String[] args) {\n' +
-        '        System.out.println("Hello, World!"); \n' +
-        '    }\n' +
-        '}'
-
-    student2.submission_time = 'December 30, 2022 at 9:37PM'
-    s2s1.QMA_scores = [10, 20, 50, 70];
-    s2s1.QMA_names = ['CC', 'Coupling', 'Cohesion', 'LOC'];
-    s2s1.CMA_scores = [45, 30, 15]
-    s2s1.CMA_names = ['Variable Names', 'Class Names', 'Comment Percentage']
-    student2.submissions.push(s2s1)
-
-    student1.submissions.push(s2s1)
-    student1.submissions.push(s2s1)
-
-    let student_list = [];
-    student_list.push(student1)
-    student_list.push(student2)
-
+    //scan each student looking for passed student ID
     for(let i = 0; i < student_list.length; i++){
         if(student_list[i].studentID == id){
-            res.render("index.ejs", {selected_student:student_list[i], student_list:student_list});
+            res.render("index.ejs", {selected_student:student_list[i],focus_file:student_list[i].submission.file_list[0], student_list:student_list});
         }
     }
+});
 
-    // let student_list = [{student_name: 'Student 1', student_id: '123'}, {student_name: 'Student 2', student_id: '456'}, {student_name: 'Student 3', student_id: '789'},{student_name: 'Student 4', student_id: '101112'}]
-    //
-    // for (let i = 0; i < student_list.length; i++){
-    //     if(student_list[i].student_id == id){
-    //         res.render("index.ejs", {selected_student:student_list[i], student_list:student_list});
-    //     }
-    // }
+app.post('/change_file', (req, res) =>{
+    let student_id = req.body.student_id;
+    console.log(student_id)
+    let file_id = req.body.file_id;
+    console.log(file_id)
 
+    //scan each student looking for passed student ID
+    for(let i = 0; i < student_list.length; i++){
+        if(student_list[i].studentID == student_id){
+            //scan each file looking for passed file ID
+            for(let j = 0; j < student_list[i].submission.file_list.length; j++){
+                if(student_list[i].submission.file_list[j].id == file_id){
+                    res.render("index.ejs", {selected_student:student_list[i],focus_file:student_list[i].submission.file_list[j], student_list:student_list});
+                }
+            }
+        }
+    }
 });
 
 app.get("/cma_summary", function(req,res){
